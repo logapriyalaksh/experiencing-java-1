a90ded71788c2f47d6f75dae5d6fa88fc8a4364a
logapriyalaksh <47312306+logapriyalaksh@users.noreply.github.com>
2019-05-03 11:28:55 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ class Leap
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        
+        int year = s.nextInt();
+        
+        if(year % 400 == 0)
+        {
+             System.out.println("yes");
+        }
+       
+        else if(year % 4 == 0 && year%100!=0)
+        {
+            System.out.println("yes");
+        }
+        
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
