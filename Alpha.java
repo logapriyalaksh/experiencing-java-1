ab2c9f4f41dc85ce038eef3234947924beb0adac
logapriyalaksh <47312306+logapriyalaksh@users.noreply.github.com>
2019-05-04 11:39:30 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,17 @@
+import java.util.*;
+  class Alpha
+
+{
+public static void main(String []args)
+{
+char ch;
+Scanner obj=new Scanner(System.in);
+
+ch=obj.next().charAt(0);
+ if(Character.isLetter(ch))
+{System.out.print("Alphabet");}
+else 
+{ System.out.print("no");}
+
+}
+}
